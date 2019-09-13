ab89e20eaaa488cd2dfa187944e705977277da04
devil87119 <40394645+devil87119@users.noreply.github.com>
2019-09-13 20:19:49 +0800

    hompage

--- js/books.js
+++ js/books.js
@@ -31,6 +31,7 @@ function list(){
 		},{
 			field: 'Item',
 			title: 'Item',
+			width: '60%',
 			switchable: true,
 			sortable: true
 		}, {
@@ -54,14 +55,114 @@ function list(){
 		}], 
 	data: book,	
 	});
-	
+	$('#books').bootstrapTable('hideColumn', 'Note');
 	$('#books').bootstrapTable('refreshOptions', {
         theadClasses: "thead-dark",
 		iconSize: 'sm'
-     })
+	})
+	$('#books').on('click-row.bs.table', function (e, row, $element) {
+		selectedRow = row;
+		/*if($($element).hasClass('custom--select') ){
+			$($element).removeClass('custom--select');
+		}
+		else{
+			$($element).addClass('custom--select');
+		}*/
+		rowStyle(row, -1);
+		//console.log(row);
+		//console.log($element);	
+		//$($element).siblings().removeClass('custom--select');      
+		//$($element).addClass('custom--select');
+	});
+}
+var checkAll = false;
+var selectedRow = {};
+
+function rowStyle(row, index) {
+    var classes = [
+      'bg_tr1',
+      'bg_tr2 ',
+    ]
+	if (index == -1) {
+		console.log(row);
+      return {
+        classes: 'custom--select'
+      }
+    }
+    else if (index % 2 === 0) {
+      return {
+        classes: classes[0]
+      }
+    }
+	else {
+		return {
+        classes: classes[1]
+      }
+	}
+  }
+
+$(function() {
+    $('#uncheckAll').click(function () {
+      checkAll = false
+      $('#table').bootstrapTable('uncheckAll')
+    })
+ })
+ 
+$(function(){//custom search 
+$(".search").append('<span class="glyphicon glyphicon-search"></span>');
+/* add the span inside search div with append box*/
+});
+ 
+function responseHandler(res) {
+    res.rows.forEach(function (item) {
+      item.state = checkAll
+    })
+	console.log(res)
+    return res
 }
 
+(function ($) {//custom word
+'use strict';
+$.fn.bootstrapTable.locales['zh-CN'] = {
+	formatLoadingMessage: function () {
+		return '正在努力地載入資料中，請稍候……';
+	},
+	formatRecordsPerPage: function (pageNumber) {
+		return '<span id = "page">每頁顯示 '+pageNumber+' 條記錄<span>';
+	},
+	formatShowingRows: function (pageFrom, pageTo, totalRows) {
+		return '<span id = "page">顯示第 ' +  pageFrom  + ' 到第 ' +  pageTo  + ' 條記錄，總共 '+   totalRows +  ' 條記錄<br><span>';
+	},
+	formatSearch: function () {
+		return '搜尋';
+	},
+	formatNoMatches: function () {
+		return '沒有找到匹配的記錄';
+	},
+	formatPaginationSwitch: function () {
+		return '隱藏/顯示分頁';
+	},
+	formatRefresh: function () {
+		return '重新整理';
+	},
+	formatToggle: function () {
+		return '切換';
+	},
+	formatColumns: function () {
+		return '列';
+	},
+	formatFullscreen: function () {
+		return '顯示欄位';
+	}
+};
+$.extend($.fn.bootstrapTable.defaults, $.fn.bootstrapTable.locales['zh-CN']);
+})(jQuery);
 
+$(function() {
+    $('#selected').click(function () {
+      alert('getSelections: ' + JSON.stringify($('#books').bootstrapTable('getSelections')))
+    })
+})
 
 function csvJSON(csv){
   var lines=csv.split("\n");
@@ -78,3 +179,33 @@ function csvJSON(csv){
   //return result; //JavaScript object
   return JSON.stringify(result); //JSON
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
